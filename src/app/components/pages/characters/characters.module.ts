216077ac30da20b63fe2d7e6521722b69d052f99
Guillermo Diaz <diazgdev@gmail.com>
2022-09-08 20:16:05 -0500

    components

--- src/app/components/pages/characters/characters.module.ts
+++ src/app/components/pages/characters/characters.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CharacterDetailsComponent } from '@characters/character-details/character-details.component';
+import { CharacterListComponent } from '@characters/character-list/character-list.component';
+//import { RouterModule } from '@angular/router';
 
-
+const myComponents = [CharacterDetailsComponent, CharacterListComponent];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [...myComponents],
+  imports: [CommonModule],
+  exports: [...myComponents]
 })
 export class CharactersModule { }
